56a242778cec449a2db8f43e0ec7d5894d4a7068
0xVedansh <vedansh83138@gmail.com>
2023-04-30 04:39:46 +0530

    api added

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
+import { exerciseOptions, fetchData } from '../utils/fetchData';  
+
 const SearchExercises = () => {
       const [search, setSearch] = useState('')
 
       const handleSearch = async () => {
         if(search){
-          // const exercisesData = await fetchData();
+          const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+          console.log(exercisesData);
         }
       }
 
@@ -18,7 +22,7 @@ const SearchExercises = () => {
         justifyContent= "center"
         p= "20px">
         <Typography
-        fontweight={700}
+        fontWeight={700}
         sx= {{ fontSize: { lg: '44px', xs: '30px'}}}
         mb="50px"
         textAlign= "center">

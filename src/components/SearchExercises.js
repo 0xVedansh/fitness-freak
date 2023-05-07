3e0e477cfe4a56dd6a6136e8e9a12221476b9540
0xVedansh <vedansh83138@gmail.com>
2023-05-08 02:39:07 +0530

    hscrollbar edited

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -78,7 +78,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
               p: '20px' }}>
             <HorizontalScrollbar data={bodyParts}
               bodyPart={bodyPart} 
-              setBodyPart={setBodyPart} />      
+              setBodyPart={setBodyPart} isBodyParts />      
         </Box>
 
       </Stack>

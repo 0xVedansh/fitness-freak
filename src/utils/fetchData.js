200a9a23961212ba2a79fa0bceffa3ea188958ab
0xVedansh <vedansh83138@gmail.com>
2023-05-09 02:44:45 +0530

    added another api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,9 +2,17 @@ export const exerciseOptions = {
     method: 'GET',
     headers: {
       'content-type': 'application/octet-stream',
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
     },
+  };
+
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+    }
   };
 
 export const fetchData = async (url, options) => {
